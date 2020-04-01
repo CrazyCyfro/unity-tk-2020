f767450c7b7b1126f7f38ce81935a1cbb017f8bf
CrazyCyfro <crazycyfro@gmail.com>
2020-04-01 13:21:16 +0800

    first commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+
+    void LateUpdate() {
+        if (Time.time > t) {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetLifespan(float lifespan) {
+        t = Time.time + lifespan;
+    }
+}
